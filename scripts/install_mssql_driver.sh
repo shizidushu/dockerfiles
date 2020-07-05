e46fbded34ff42222807b188777c9ba8a237e7e6
shizidushu <shizidushu@gmail.com>
2020-07-05 14:47:36 +0800

    try test

--- scripts/install_mssql_driver.sh
+++ scripts/install_mssql_driver.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-set -e
 
 # https://docs.microsoft.com/en-us/sql/connect/odbc/linux-mac/installing-the-microsoft-odbc-driver-for-sql-server?view=sql-server-ver15#ubuntu17
 
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list
-
-apt-get update
-ACCEPT_EULA=Y apt-get install msodbcsql17
+echo 'testing'
