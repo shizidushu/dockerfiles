2a9e7164a8c57777cbca14819d4ddfa3331bc147
shizidushu <shizidushu@gmail.com>
2020-10-15 21:11:18 +0800

    url

--- scripts/upgrade_to_rstudio_preview.sh
+++ scripts/upgrade_to_rstudio_preview.sh
@@ -3,6 +3,13 @@ set -e
 
 DOWNLOAD_VERSION=`wget -qO - https://rstudio.com/products/rstudio/download/preview/ | grep -oP "(?<=rstudio-server-)[0-9]\.[0-9]\.[0-9]+" | sort | tail -n 1`
 
+
+## UBUNTU_VERSION is not generally valid: only works for xenial and bionic, not other releases,
+## and does not understand numeric versions. (2020-04-15)
+#RSTUDIO_URL="https://s3.amazonaws.com/rstudio-ide-build/server/${UBUNTU_VERSION}/amd64/rstudio-server-${DOWNLOAD_VERSION}-amd64.deb"
+## hardwire bionic for now...
+RSTUDIO_URL="https://s3.amazonaws.com/rstudio-ide-build/server/bionic/amd64/rstudio-server-${DOWNLOAD_VERSION}-amd64.deb"
+
 if [ "$UBUNTU_VERSION" = "xenial" ]; then
   wget $RSTUDIO_URL || \
   wget `echo $RSTUDIO_URL | sed 's/server-/server-xenial-/'` || \
