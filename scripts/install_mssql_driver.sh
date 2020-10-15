17674de7258c1dba4560586feaf2aa5a06976a7f
shizidushu <shizidushu@gmail.com>
2020-10-15 21:17:54 +0800

    fix mssql driver

--- scripts/install_mssql_driver.sh
+++ scripts/install_mssql_driver.sh
@@ -5,4 +5,4 @@
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list
 apt-get update
-ACCEPT_EULA=Y apt-get install -y unixodbc unixodbc-dev msodbcsql17
+ACCEPT_EULA=Y apt-get install -y mssql-tools unixodbc-dev odbcinst1debian2 msodbcsql17 unixodbc libodbc1
